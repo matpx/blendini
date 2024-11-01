e532bbc8c9d89bad3dd40223a5d190ed9e9d0068
matpx <9219741+matpx@users.noreply.github.com>
2024-11-01 17:03:13 +0100

    run

--- Makefile
+++ Makefile
@@ -1,4 +1,15 @@
 
+RAYLIB_SRC = raylib/src/
 
-all: raylib
+blendini: blendini.cpp $(RAYLIB_SRC)libraylib.a
+	$(CXX) -o blendini blendini.cpp -std=c++20 -Wall -Wextra -I$(RAYLIB_SRC) $(RAYLIB_SRC)libraylib.a
+
+$(RAYLIB_SRC)libraylib.a:
+	$(MAKE) -C $(RAYLIB_SRC)
+
+.PHONY: clean
+
+clean:
+	$(RM) blendini blendini.exe
+	$(MAKE) -C $(RAYLIB_SRC) clean
 
---
+++ blendini.cpp
@@ -0,0 +1,58 @@
+
+#include <raylib.h>
+
+int main(void) {
+  const int screenWidth = 1920;
+  const int screenHeight = 1080;
+
+  SetConfigFlags(FLAG_VSYNC_HINT | FLAG_MSAA_4X_HINT);
+  InitWindow(screenWidth, screenHeight,
+             "raylib [core] example - 3d camera free");
+
+  SetTargetFPS(60);
+  EnableEventWaiting();
+
+  Camera3D camera = {
+      .position = Vector3{10.0f, 10.0f, 10.0f},
+      .target = Vector3{0.0f, 0.0f, 0.0f},
+      .up = Vector3{0.0f, 1.0f, 0.0f},
+      .fovy = 45.0f,
+      .projection = CAMERA_PERSPECTIVE,
+  };
+
+  const Vector3 cubePosition = {0.0f, 0.0f, 0.0f};
+
+  while (!WindowShouldClose()) {
+    UpdateCamera(&camera, CAMERA_FREE);
+
+    if (IsKeyPressed('Z'))
+      camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    BeginMode3D(camera);
+
+    DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+    DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+
+    DrawGrid(10, 1.0f);
+
+    EndMode3D();
+
+    DrawRectangle(10, 10, 320, 93, Fade(SKYBLUE, 0.5f));
+    DrawRectangleLines(10, 10, 320, 93, BLUE);
+
+    DrawText("Free camera default controls:", 20, 20, 10, BLACK);
+    DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
+    DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
+    DrawText("- Z to zoom to (0, 0, 0)", 40, 80, 10, DARKGRAY);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
+}
